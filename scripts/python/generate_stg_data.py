2f8db42c0018fae3a637853c878b33fbcfc57647
maintheme2 <oligarh20031@mail.ru>
2025-05-09 01:15:35 +0300

    add dag run logs

--- scripts/python/generate_stg_data.py
+++ scripts/python/generate_stg_data.py
@@ -1,10 +1,12 @@
 import random
 import pandas as pd
 from faker import Faker
+from datetime import datetime
 
 fake = Faker()
 
-def generate_data() -> None:
+def generate_data(run_id, **context) -> None:
+
     customers_data = {
         "customer_id": [],
         "name": [],
@@ -28,7 +30,13 @@ def generate_data() -> None:
         "product_id": [],
     }
 
-    for _ in range(1000):
+    f = open("logs/generate_stg_data_logs.txt", "a")
+    f.write('--------------------------------------------------------')
+    f.write(f"\nSTARTED DAG RUN {run_id}\n")
+    f.write('--------------------------------------------------------')
+
+    num_rows = 10**5 + fake.random_int(min=1000, max=12000) - fake.random_int(min=1000, max=12000)
+    for i in range(1, num_rows):
         customers_data["customer_id"].append(fake.random_int(min=100, max=10**10))
         customers_data["name"].append(fake.name())
         customers_data["address"].append(fake.address())
@@ -45,7 +53,16 @@ def generate_data() -> None:
         orders_data["order_date"].append(fake.date_time())
         orders_data["customer_id"].append(fake.random_choices(customers_data["customer_id"])[0])
         orders_data["product_id"].append(fake.random_choices(products_data["product_id"])[0])
+
+        if i % 1000 == 0 or i == num_rows-1:
+            if i == num_rows-1: f.write(f"\n{datetime.now()}: generation finished, rows in total: {i} \n")
+            else: f.write(f"\n{datetime.now()}: generated {i} rows")
     
+    f.write('---------------------------------------------------------')
+    f.write(f"\nDAG RUN {run_id} FINISHED\n")
+    f.write('---------------------------------------------------------\n')
+    f.close()
+
     customers_df = pd.DataFrame(customers_data)
     products_df = pd.DataFrame(products_data)
     orders_df = pd.DataFrame(orders_data)
