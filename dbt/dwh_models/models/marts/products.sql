351777226d3e94e43d11ce67e4b9fa93ce70d9dd
maintheme2 <oligarh20031@mail.ru>
2025-05-09 01:14:14 +0300

    add new dbt models

---
+++ dbt/dwh_models/models/marts/products.sql
@@ -0,0 +1,12 @@
+{{
+    config(
+        materialized = 'incremental',
+    )
+}}
+
+select stg.order_id,
+       stg.order_date,
+       stg.customer_id,
+       stg.product_id,
+       current_timestamp as as_of_day
+from store_stg.stg_products stg
