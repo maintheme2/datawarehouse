05fe00a56333123a9e6358f48dfe38d63b728760
maintheme2 <oligarh20031@mail.ru>
2025-05-11 20:03:28 +0300

    generilize script to match all tables

--- scripts/python/generate_inserts.py
+++ scripts/python/generate_inserts.py
@@ -10,11 +10,10 @@ def generate_inserts(**kwargs) -> None:
     
     insert_queries = []
     for index, row in df.iterrows():
-        insert_query = f"INSERT INTO {target_schema_name}.{target_table_name} VALUES ({row['order_id']}, '{row['order_date']}', {row['customer_id']}, {row['product_id']}, '{datetime.now()}');"
+        insert_query = f"INSERT INTO {target_schema_name}.{target_table_name} VALUES ({', '.join(f"'{value}'" for value in row.values)}, '{datetime.now()}');"
         insert_queries.append(insert_query)
     
     # Save queries to a file for the PostgresOperator to execute
     with open(f'/opt/scripts/sql/insert_{target_schema_name}__{target_table_name}.sql', 'w') as f:
         for query in insert_queries:
             f.write(f"{query}\n")
-
