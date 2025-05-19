57d90fe697543246490a00586eacc563013ce6bc
maintheme2 <oligarh20031@mail.ru>
2025-05-19 22:26:50 +0300

    dag daily_orders

---
+++ airflow/dags/DAG_STORE_DWH__DAILY_ORDERS.py
@@ -0,0 +1,29 @@
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
+from datetime import datetime
+
+
+with DAG (
+    dag_id = 'DAG_STORE_DWH__DAILY_ORDERS',
+    description = 'Aggregated data to support daily-level reporting',
+    schedule_interval = '@daily',
+    start_date = datetime(2025, 2, 23),
+    catchup = False
+) as dag:
+
+    dbt_daily_orders = BashOperator(
+        task_id = 'dbt_daily_orders',
+        bash_command = 'dbt run --project-dir /opt/dbt/dwh_models --profiles-dir /opt/dbt/ --select daily_orders'
+    )
+
+    wait_dwh_orders = ExternalTaskSensor(
+        task_id='wait_dwh_orders',
+        external_dag_id='DAG_STORE_DWH__ORDERS',
+        external_task_id='dbt_orders',
+        poke_interval=5*60,
+        timeout=86400+100,
+        mode='reschedule'
+    )
+
+    wait_dwh_orders >> dbt_daily_orders
