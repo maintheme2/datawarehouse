b208b9b67bf0b3cbc74e791367c7af69236e8fdc
maintheme2 <oligarh20031@mail.ru>
2025-05-19 22:22:46 +0300

    on conflict with key columns do nothing

--- scripts/python/generate_inserts.py
+++ scripts/python/generate_inserts.py
@@ -8,9 +8,17 @@ def generate_inserts(**kwargs) -> None:
 
     df = pd.read_csv(f'/opt/data/generated/{file_name}')
     
+    if target_table_name == 'stg_orders':
+        key_name = 'order_id' 
+    elif target_table_name == 'stg_products':
+        key_name = 'product_id'
+    elif target_table_name == 'stg_customers':
+        key_name = 'customer_id'
+
     insert_queries = []
     for index, row in df.iterrows():
-        insert_query = f"INSERT INTO {target_schema_name}.{target_table_name} VALUES ({', '.join(f"'{value}'" for value in row.values)}, '{datetime.now()}');"
+        insert_query = f"INSERT INTO {target_schema_name}.{target_table_name} VALUES
+            ({', '.join(f"'{value}'" for value in row.values)}, '{datetime.now()}') ON CONFLICT ({key_name}) DO NOTHING;"
         insert_queries.append(insert_query)
     
     # Save queries to a file for the PostgresOperator to execute
